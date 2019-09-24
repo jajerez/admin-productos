156c618a2fe511be5cb5a8e2aeee4b708d4a2e88
jajerez <54459285+jajerez@users.noreply.github.com>
2019-09-24 18:10:22 -0300

    Update product.js

--- models/product.js
+++ models/product.js
@@ -6,7 +6,6 @@ const Schema = mongoose.Schema
 const ProductSchema = Schema({
     "nombre": String,
     "precio": { type: Number, default:0},
-    //"categoria": { type: String, enum: ['computador', 'audifonos', 'accesorio'] },
     "categoria": String,
 	"descripcion": String
 
